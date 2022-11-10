2ec99add292296ad40c009bd693a9c80ad59d809
Hessel <hesselwerks@proton.me>
2022-11-10 05:34:30 -0700

    Added multi-jump, encapsulated movement code a bit further.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,13 +2,19 @@ extends CharacterBody3D
 
 
 var player_speed_current: float = 0.0
-@export var player_speed_walk_max: float = 5.0
+@export var player_speed_walk_max: float = 6.0
 @export var player_speed_sprint_max: float = player_speed_walk_max * 2.0
 @export var player_walk_accel_rate: float = 4
 @export var player_sprint_accel_rate: float = 7
-@export var player_decel_rate: float = 8
+@export var player_decel_rate: float = 10
 @export var player_rotation_rate: float = 9.0
-@export var jump_velocity: float = 4.5
+@export var jump_velocity: float = 7
+@export var max_jumps: int = 2
+var jumps_remaining: int = max_jumps
+var is_jumping: bool = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @export var mouse_sensitivity: float = 2.5
 @export var joystick_sensitivity: float = 0.05
@@ -21,26 +27,60 @@ enum PlayerMovementState {
 
 var movement_state: PlayerMovementState = PlayerMovementState.IDLE
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	
 	# capture mouse movement for camera navigation
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
+#fluctuating framerate-based delta time
+func _process(delta: float) -> void:
+	pass
+
+
+# difference from _process: https://godotengine.org/qa/57458/difference-between-_process-_physics_process-have-script
+#tl;dr stable delta time
 func _physics_process(delta: float) -> void:
-	### MOVEMENT ###
 	
-	# Apply gravity when in midair.
+	# gravity, then jumping, in that order
+	apply_jump_and_gravity(delta)
+
+	# Lateral movement - gets direction vector from inputs, calls funcs to determine speed (or lack thereof) and applies movement.
+	apply_player_lateral_movement(delta)
+	
+	# Camera
+	rotate_cam_joypad()
+
+	# Collisions
+	@warning_ignore(return_value_discarded) # makes debugger shut up
+	# processes complex collisions: see https://godotengine.org/qa/44624/kinematicbody3d-move_and_slide-move_and_collide-different
+	move_and_slide()
+	
+	
+func _input(event):
+	# Camera
+	rotate_cam_kb_m(event)
+	
+	
+func apply_jump_and_gravity(delta) -> void:
+	
+	# Apply gravity, reset jumps
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	else:
+		if is_jumping:
+			is_jumping = false
+			jumps_remaining = max_jumps
 
-	# Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Jumping
+	if Input.is_action_just_pressed("jump") && jumps_remaining > 0:
+		jumps_remaining -= 1
+		is_jumping = true
 		velocity.y = jump_velocity
-
+	
+	
+func apply_player_lateral_movement(delta) -> void:
+	
 	# Get the input direction.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	# set direction to always face forward, relative to where the camera's spring arm is positioned. note: this makes movement slower when looking down w/ cam
@@ -48,7 +88,7 @@ func _physics_process(delta: float) -> void:
 	
 	# if we have input, do something.
 	if direction:
-		calculate_player_movement(delta)
+		calculate_player_speed(delta)
 		
 		# apply movement
 		velocity.x = direction.x * player_speed_current
@@ -62,20 +102,8 @@ func _physics_process(delta: float) -> void:
 	# debug
 	#print(movement_state)
 	
-	# Camera
-	rotate_cam_joypad()
 
-	# processes complex collisions: see https://godotengine.org/qa/44624/kinematicbody3d-move_and_slide-move_and_collide-different
-	@warning_ignore(return_value_discarded) # makes debugger shut up
-	move_and_slide()
-	
-	
-func _input(event):
-	# Camera
-	rotate_cam_kb_m(event)
-	
-
-func calculate_player_movement(delta) -> void:
+func calculate_player_speed(delta) -> void:
 	
 	# determine movement state (sprinting or walking)
 	if (Input.is_action_pressed("sprint") and is_on_floor()):
