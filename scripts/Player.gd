81ae95f0dddfdc416d7575120188d23a8a2ac402
Hessel <hesselwerks@proton.me>
2022-11-10 04:55:34 -0700

    Refactored a lot of movement code into separate functions.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -9,6 +9,7 @@ var player_speed_current: float = 0.0
 @export var player_decel_rate: float = 8
 @export var player_rotation_rate: float = 9.0
 @export var jump_velocity: float = 4.5
+
 @export var mouse_sensitivity: float = 2.5
 @export var joystick_sensitivity: float = 0.05
 
@@ -30,7 +31,6 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
-	
 	### MOVEMENT ###
 	
 	# Apply gravity when in midair.
@@ -40,7 +40,6 @@ func _physics_process(delta: float) -> void:
 	# Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
-		
 
 	# Get the input direction.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
@@ -49,39 +48,62 @@ func _physics_process(delta: float) -> void:
 	
 	# if we have input, do something.
 	if direction:
-		
-		# determine movement state (sprinting or walking)
-		if (Input.is_action_pressed("sprint") and is_on_floor()):
-			movement_state = PlayerMovementState.SPRINT
-		else:
-			movement_state = PlayerMovementState.WALK
-		
-		# player mesh rotation relative to camera. note: the entire player never rotates: only the spring arm or the mesh.
-		if $PlayerMeshCapsule.rotation.y != $SpringArm3D.rotation.y:
-			# rotate the player's mesh instead of the entire Player; rotating that will move the camera, too.
-			$PlayerMeshCapsule.rotation.y = move_toward($PlayerMeshCapsule.rotation.y, $SpringArm3D.rotation.y, player_rotation_rate * delta)
-		
-		# acceleration, based on movement state
-		if (movement_state == PlayerMovementState.WALK):
-			if player_speed_current < player_speed_walk_max:
-				player_speed_current += (player_walk_accel_rate * delta)
-			elif player_speed_current > player_speed_walk_max:
-				player_speed_current = player_speed_walk_max
-				
-		elif (movement_state == PlayerMovementState.SPRINT):
-			if player_speed_current < player_speed_sprint_max:
-				player_speed_current += (player_sprint_accel_rate * delta)
-			elif player_speed_current > player_speed_sprint_max:
-				player_speed_current = player_speed_sprint_max
+		calculate_player_movement(delta)
 		
 		# apply movement
 		velocity.x = direction.x * player_speed_current
 		velocity.z = direction.z * player_speed_current
 		
-		
 	# if there's no input, decelerate or do nothing.
 	else:
+		stop_player_movement(delta)
+	# debug
+	#print(player_speed_current)
+	# debug
+	#print(movement_state)
+	
+	# Camera
+	rotate_cam_joypad()
+
+	# processes complex collisions: see https://godotengine.org/qa/44624/kinematicbody3d-move_and_slide-move_and_collide-different
+	@warning_ignore(return_value_discarded) # makes debugger shut up
+	move_and_slide()
+	
+	
+func _input(event):
+	# Camera
+	rotate_cam_kb_m(event)
+	
+
+func calculate_player_movement(delta) -> void:
+	
+	# determine movement state (sprinting or walking)
+	if (Input.is_action_pressed("sprint") and is_on_floor()):
+		movement_state = PlayerMovementState.SPRINT
+	else:
+		movement_state = PlayerMovementState.WALK
 		
+	# player mesh rotation relative to camera. note: the entire player never rotates: only the spring arm or the mesh.
+	if $PlayerMeshCapsule.rotation.y != $SpringArm3D.rotation.y:
+		# rotate the player's mesh instead of the entire Player; rotating that will move the camera, too.
+		$PlayerMeshCapsule.rotation.y = move_toward($PlayerMeshCapsule.rotation.y, $SpringArm3D.rotation.y, player_rotation_rate * delta)
+		
+	# acceleration, based on movement state
+	if (movement_state == PlayerMovementState.WALK):
+		if player_speed_current < player_speed_walk_max:
+			player_speed_current += (player_walk_accel_rate * delta)
+		elif player_speed_current > player_speed_walk_max:
+			player_speed_current = player_speed_walk_max
+				
+	elif (movement_state == PlayerMovementState.SPRINT):
+		if player_speed_current < player_speed_sprint_max:
+			player_speed_current += (player_sprint_accel_rate * delta)
+		elif player_speed_current > player_speed_sprint_max:
+			player_speed_current = player_speed_sprint_max
+	
+	
+func stop_player_movement(delta) -> void:
+	
 		# update movement state
 		movement_state = PlayerMovementState.IDLE
 		
@@ -91,33 +113,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, player_decel_rate * delta)
 		velocity.z = move_toward(velocity.z, 0, player_decel_rate * delta)
 		#velocity = velocity.move_toward(Vector3.ZERO, player_decel_rate * delta)
-		
-	# debug
-	#print(player_speed_current)
-	
-	# debug
-	print(movement_state)
 	
 	
-	### CAMERA ###
-		
-	# controller spring arm rotation
-	$SpringArm3D.rotation.y -= Input.get_action_strength("camera_left_joystick") * -joystick_sensitivity
-	$SpringArm3D.rotation.y -= Input.get_action_strength("camera_right_joystick") * joystick_sensitivity
-	
-	$SpringArm3D.rotation.x -= Input.get_action_strength("camera_up_joystick") * -joystick_sensitivity
-	$SpringArm3D.rotation.x -= Input.get_action_strength("camera_down_joystick") * joystick_sensitivity
-	$SpringArm3D.rotation.x = clamp($SpringArm3D.rotation.x, -1.4, 0.3)
-
-	# processes complex collisions: see https://godotengine.org/qa/44624/kinematicbody3d-move_and_slide-move_and_collide-different
-	@warning_ignore(return_value_discarded) # makes debugger shut up
-	move_and_slide()
-	
-	
-func _input(event):
-	
-	### CAMERA ###
-	
+func rotate_cam_kb_m(event) -> void:
 	# mouse spring arm rotation
 	if (event is InputEventMouseMotion):
 		# mouse x movement (in 2d monitor space) becomes spring arm rotation in 3D space around the Y axis - left/right rotation.
@@ -130,6 +128,13 @@ func _input(event):
 		#print($SpringArm3D.rotation.x)
 		
 
-
+func rotate_cam_joypad() -> void:
+	# controller spring arm rotation
+	$SpringArm3D.rotation.y -= Input.get_action_strength("camera_left_joystick") * -joystick_sensitivity
+	$SpringArm3D.rotation.y -= Input.get_action_strength("camera_right_joystick") * joystick_sensitivity
+	
+	$SpringArm3D.rotation.x -= Input.get_action_strength("camera_up_joystick") * -joystick_sensitivity
+	$SpringArm3D.rotation.x -= Input.get_action_strength("camera_down_joystick") * joystick_sensitivity
+	$SpringArm3D.rotation.x = clamp($SpringArm3D.rotation.x, -1.4, 0.3)
 
 
