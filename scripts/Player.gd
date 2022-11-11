62b60a528360565916700b7489e56018497f8d39
Hessel <hesselwerks@proton.me>
2022-11-11 12:57:05 -0700

    Added initial air drag implementation. May need some tweaking in the future, but working fairly well.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,12 +6,16 @@ var player_speed_current: float = 0.0
 @export var player_speed_sprint_max: float = player_speed_walk_max * 2.0
 @export var player_walk_accel_rate: float = 4
 @export var player_sprint_accel_rate: float = 7
-@export var player_decel_rate: float = 12
+@export var player_decel_rate: float = 14
 @export var player_rotation_rate: float = 9.0
 @export var jump_velocity: float = 7
+@export var air_drag_percentage_max: float = 0.75
+var air_drag_percentage: float = air_drag_percentage_max
 @export var max_jumps: int = 2
 var jumps_remaining: int = max_jumps
+var midair_direction_changes: int = 0
 var is_jumping: bool = false
+var ground_dir_cache: Vector3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -67,10 +71,18 @@ func apply_jump_and_gravity(delta) -> void:
 	# Apply gravity, reset jumps
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		
+		# check for directional switches in midair
+		var inputting_movement: bool = Input.is_action_just_released("forward") || Input.is_action_just_released("backward") || Input.is_action_just_released("left") || Input.is_action_just_released("right")
+		if (inputting_movement):
+			midair_direction_changes += 1
+		
 	else:
 		if is_jumping:
 			is_jumping = false
 			jumps_remaining = max_jumps
+			midair_direction_changes = 0
+			air_drag_percentage = air_drag_percentage_max
 
 	# Jumping
 	if Input.is_action_just_pressed("jump") && jumps_remaining > 0:
@@ -104,9 +116,28 @@ func apply_player_lateral_movement(delta) -> void:
 	if direction:
 		calculate_player_speed(delta)
 		
-		# apply movement
-		velocity.x = direction.x * player_speed_current
-		velocity.z = direction.z * player_speed_current
+		# check if the player is moving laterally. used for air-drag in midair, probably redundant when used in conjunction with cached ground direction.
+		var inputting_movement: bool = Input.is_action_pressed("forward") || Input.is_action_pressed("backward") || Input.is_action_pressed("left") || Input.is_action_pressed("right")
+		
+		# calculate air drag
+		# mild air drag that doesn't worsen is applied if the player rotates the view while jumping.
+		# switching input directions will worsen air drag each time it occurs, to a point.
+		air_drag_percentage = air_drag_percentage_max - (midair_direction_changes * 0.1)
+		air_drag_percentage = clamp(air_drag_percentage, 0.3, air_drag_percentage_max)
+		
+		# apply movement, apply air drag if there is any
+		if (is_jumping && inputting_movement && direction != ground_dir_cache):
+			velocity.x = direction.x * player_speed_current * air_drag_percentage
+			velocity.z = direction.z * player_speed_current * air_drag_percentage
+			
+			if (ground_dir_cache != Vector3(1, 1, 1)):
+				ground_dir_cache = Vector3(1, 1, 1) # in case player switches back to same direction again while in midair
+		else:
+			velocity.x = direction.x * player_speed_current
+			velocity.z = direction.z * player_speed_current
+			
+			# cache direction every time the player moves on the ground, for air drag application when jumping
+			ground_dir_cache = direction
 		
 		# player mesh rotation relative to camera. note: the entire Player never rotates: only the spring arm or the mesh.
 		if $PlayerMeshCapsule.rotation.y != $SpringArm3D.rotation.y:
